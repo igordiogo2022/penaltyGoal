0327f33a518f52689aa1c1b8a443c0a52e0cec2a
Igor Diogo <igordiogo1314@gmail.com>
2025-03-31 18:07:41 -0300

    Adição dos nome de jogadores

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+const listaTimes = [['sccp', 'Memphis Depay','Yuri Alberto', 'Rodrigo Garro','Carrilho','Raniele','Hugo Souza'],
+['cep','Raphael Veiga','Estêvão','Richard Rios','Vitor Roque','Felipe Anderson','Weverton'],
+['spfc','Lucas Moura','Oscar','Luciano','Calleri','Luiz Gustavo','Rafael'],
+['santos','Neymar', 'Guilherme', 'Soteldo', 'Thacianno', 'João Schmidt', 'Gabriel Brazão'],
+['fla','Pedro', 'Arrascaeta', 'Gerson', 'De La Cruz', 'Wesley', 'Rossi'],
+['flu','Germán Cano', 'John Arias', 'Ganso', 'Martinelli', 'Canobbio', 'Fábio'],
+['btfg','Igor Jesus', 'Savarino', 'Marlon Freitas', 'Gregore', 'Alexander Barboza', 'John'],
+['vasco','Pablo Vegetti', 'P. Coutinho', 'Hugo Moura', 'Nuno Moreira', 'Lucas Piton', 'Leo Jardim'],
+['inter','Alan Patrick', 'Enner Valencia', 'Wesley', 'Bruno Henrique', 'Vitão', 'Anthoni'],
+['gremio','Braithwaite', 'Edenílson', 'Villlasanti', 'Monsalve', 'Cristian Pavón', 'Tiago Volpi'],
+['cruzeiro','Gabriel', 'Dudu', 'Matheus Pereira', 'Matheus Henrique', 'Kaio Jorge', 'Cássio'],
+['atl_mg','Hulk', 'Rony', 'Gustavo Scarpa', 'Gabriel Menino', 'Guilherme Arana', 'Everson'],
+['ath_pr','Zapelli', 'Luiz Fernando', 'Velasco', 'Felipinho', 'Raul', 'Mycael'],
+['coxa','Josué', 'Dellatorre', 'Ronier', 'Coutinho', 'Rafinha', 'Pedro Morisco'],
+['fort','Lucero', 'Marinho', 'Yago Pikachu', 'Lucas Sasha', 'Emmanuel Martínez', 'João Ricardo'],
+['ceara','Pedro Raul', 'Fernando Sobral', 'Aylon', 'Fernandinho', 'Lucas Mugni', 'Bruno'],
+['bahia','Everton Ribeiro', 'Luciano Rodríguez', 'Erick Pulga', 'Jean Lucas', 'Caio Alexandre', 'Ronaldo'],
+['vito','Wellington Rato', 'Gustavo Mosquito', 'Matheuzinho', 'Ronald', 'Lucas Halter', 'Lucas Arcanjo'],
+['sport','Lucas Lima', 'Pablo', 'Du Queiroz', 'Sérgio Oliveira', 'Hereda', 'Caique'],
+['rbb','Jhon Jhon', 'Thiago Borbas', 'Matheus Fernandes', 'Juninho Capixaba', 'Laquitana', 'Cleiton']];
+
 var vez = 'player';
 var golsPlayer = 0;
 var golsAdversario = 0;
@@ -12,6 +33,15 @@ var a = 1;
 function obterId(timeId){
     localStorage.setItem('timePlayer', timeId);
     localStorage.setItem('faseCdb', 16);
+}
+
+function obterIdNumerico(timePlayerId){
+    for(let i=0;i<listaTimes.length;i++){
+        var idTime = listaTimes[i][0];
+        if(idTime == timePlayerId){
+            return i;
+        }
+    }
 }
 
 function carregarImagensTimes(classeId){
@@ -107,20 +137,19 @@ function cdbOitavas(){
     var timePlayerId = localStorage.getItem('timePlayer');
     
     //uma lista normal e uma embaralhada
-    const listaTimes = ['sccp','cep','spfc','santos','fla','flu','btfg','vasco','inter','gremio','cruzeiro','atl_mg','ath_pr','coxa','fort','ceara','bahia','vito','sport','rbb'];
     const listaTimesSorteada = [];
     
     //o time do player é adicionado como primeiro e excluido da lista times para evitar repetições
+    idNumerico = obterIdNumerico(timePlayerId);
     listaTimesSorteada.push(timePlayerId);
-    var indexT = listaTimes.indexOf(timePlayerId);
-    listaTimes.splice(indexT, 1);
+    listaTimes.splice(idNumerico, 1);
     
     //embaralha os times
     for(let i=14; i>=0; i--){
         //sorteia um numero que será o index do item
         index = Math.round(Math.random()*(listaTimes.length-1));
         //adiciona o item a lista nova
-        listaTimesSorteada.push(listaTimes[index]);
+        listaTimesSorteada.push(listaTimes[index][0]);
         
         //exclui o item da lista antiga para evitar repetições
         listaTimes.splice(index, 1);
@@ -192,7 +221,11 @@ function cdbCampeao(){
 
 function timesJogo(){
     times = document.querySelectorAll('.time');
+    nomeBatedor = document.querySelector('#nomeBatedor');
+    idPlayer = obterIdNumerico(localStorage.getItem('timePlayer'));
     
+    nomeBatedor.innerHTML = listaTimes[idPlayer][1];
+
     times[0].id = localStorage.getItem('timePlayer');
     times[1].id = localStorage.getItem('adversario');
     times[2].id = localStorage.getItem('timePlayer');
@@ -204,24 +237,30 @@ function timesJogo(){
 function mudarVez(){
     frase = document.querySelector('#fraseVez');
     placar = document.querySelectorAll('.contagemAcertos');
-    
+    nomeBatedor = document.querySelector('#nomeBatedor');
+
+    idPlayer = obterIdNumerico(localStorage.getItem('timePlayer'));
+    idAdversario = obterIdNumerico(localStorage.getItem('adversario'));
+
     if(vez=='player'){
         vez='adversario';
         frase.innerHTML = 'ESCOLHA UM LUGAR PARA DEFENDER';
+        nomeBatedor.innerHTML = listaTimes[idAdversario][((qtdAtualBatidas)%5)+1];
     }else if(vez=='adversario'){
         vez='player';
         frase.innerHTML = 'ESCOLHA UM LUGAR PARA CHUTAR';
+        nomeBatedor.innerHTML = listaTimes[idPlayer][((qtdAtualBatidas+0.5)%5)+1];
     }
     placar[0].innerHTML = contagemPlayer;
     placar[1].innerHTML = contagemAdversario;
     qtdAtualBatidas = qtdAtualBatidas + 0.5;
-    
 }
 
 function analisarChute(posPlayer){
     narracao = document.querySelector('#narracaoTexto');
 
     posAdversario = Math.round(Math.random()*5)+1;
+    posAdversario = 1;
     
     if(vez=='player'){
         if(qtdGeralBatidas!=5 && (qtdGeralBatidas-1)%5==0){
@@ -254,9 +293,8 @@ function analisarChute(posPlayer){
         }
     }
 
-    
-    
     if(qtdAtualBatidas>=4.5 && golsAdversario==golsPlayer){
+        
         qtdGeralBatidas++;
         desempate = 'ativado';
     }
