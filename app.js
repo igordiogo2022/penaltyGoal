694ee0a0824c26ebcfe489bf22ccafee8fe79c64
Igor Diogo <igordiogo1314@gmail.com>
2025-04-01 10:52:39 -0300

    Comentando o código

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//lista de times e seus jogadores
 const listaTimes = [['sccp', 'Memphis Depay','Yuri Alberto', 'Rodrigo Garro','Carrilho','Raniele','Hugo Souza'],
 ['cep','Raphael Veiga','Estêvão','Richard Rios','Vitor Roque','Felipe Anderson','Weverton'],
 ['spfc','Lucas Moura','Oscar','Luciano','Calleri','Luiz Gustavo','Rafael'],
@@ -19,6 +20,7 @@ const listaTimes = [['sccp', 'Memphis Depay','Yuri Alberto', 'Rodrigo Garro','Ca
 ['sport','Lucas Lima', 'Pablo', 'Du Queiroz', 'Sérgio Oliveira', 'Hereda', 'Caique'],
 ['rbb','Jhon Jhon', 'Thiago Borbas', 'Matheus Fernandes', 'Juninho Capixaba', 'Laquitana', 'Cleiton']];
 
+//determina de quem a vez de chutar
 var vez = 'player';
 var golsPlayer = 0;
 var golsAdversario = 0;
@@ -27,14 +29,17 @@ var contagemAdversario = '';
 var qtdAtualBatidas = 0;
 var qtdGeralBatidas = 5;
 var desempate = 'desativado';
+//valor que é 1 caso o time player esteja jogando e 0 caso ele tenha sido eliminado
+//usado para cálculo dos jogos entre a própria máquina
 var a = 1;
 
-
+//obtem o id do time escolhido e inicia a cdb
 function obterId(timeId){
     localStorage.setItem('timePlayer', timeId);
     localStorage.setItem('faseCdb', 16);
 }
 
+//busca pelo id do time o lugar na lista de time 
 function obterIdNumerico(timePlayerId){
     for(let i=0;i<listaTimes.length;i++){
         var idTime = listaTimes[i][0];
@@ -48,12 +53,13 @@ function carregarImagensTimes(classeId){
     //obtem todos com a classe
     times = document.querySelectorAll(classeId);
 
-    //para cada time insere a foto do time pegando o id
+    //para cada time insere a foto do time pelo o id
     for(const time of times){
         time.style.backgroundImage = "url('/Times/"+time.id+".png')";
     } 
 }
 
+//indica qual fase da cdb está
 function cdb(){
     fase = localStorage.getItem('faseCdb');
     localStorage.setItem('faseCdb', fase/2);
@@ -75,43 +81,50 @@ function carregarTimesCdb(){
     }
 }
 
+
 function passarFazerCdb(){
     const listaTimes = localStorage.getItem('listaTimes').split(',');
     const listaClassificados = [];
-    
-    vencedorJogo = localStorage.getItem('vencedorJogo');
+    var vencedorJogo = localStorage.getItem('vencedorJogo');
+
+    //verifica o vencedor do jogo do player e classifica o vencedor
     if(vencedorJogo=='player'){
         listaClassificados.push(localStorage.getItem('timePlayer'));
     }else if(vencedorJogo=='adversario' && localStorage.getItem('status')=='competindo'){
         listaClassificados.push(localStorage.getItem('adversario'));
     }
     
+    //repete todas as fases que o player não está
     if(localStorage.getItem('status')=='eliminado'){
         a=0;
     }
     
+    //simula as partidas entre a própria máquina
+    //quando 'a' é 1 ignora a primeira partida(partida do player)
     for(let i=a;i<(listaTimes.length/2);i++){
-        golsDono = Math.round(Math.random()*5);
+        golsMandante = Math.round(Math.random()*5);
         golsVisitante = Math.round(Math.random()*5);
         
-        
-        if(golsDono==golsVisitante){
+        //caso empate realiza um desempate
+        if(golsMandante==golsVisitante){
             desempate = Math.round(Math.random()*1);
             
             if(desempate==0){
-                golsDono++;
+                golsMandante++;
             }else if(desempate==1){
                 golsVisitante++;
             }
         }
-        
-        if(golsDono>golsVisitante){
+
+        //verifica o vencedor
+        if(golsMandante>golsVisitante){
             listaClassificados.push(listaTimes[i*2]);
-        }else if(golsVisitante>golsDono){
+        }else if(golsVisitante>golsMandante){
             listaClassificados.push(listaTimes[i*2+1]);
         }
     }
-    
+
+    //eleimina o player caso ele tenha perdido
     if(vencedorJogo=='adversario'){
         localStorage.setItem('status', 'eliminado');
         botao = document.querySelector('#botao');
@@ -120,7 +133,6 @@ function passarFazerCdb(){
     }
     
     times = document.querySelectorAll(".opcao");
-    
     //adiciona o id na ordem sorteada
     for(let i=0;i<listaClassificados.length;i++){
         times[i].id = listaClassificados[i];
@@ -135,8 +147,6 @@ function passarFazerCdb(){
 
 function cdbOitavas(){
     var timePlayerId = localStorage.getItem('timePlayer');
-    
-    //uma lista normal e uma embaralhada
     const listaTimesSorteada = [];
     
     //o time do player é adicionado como primeiro e excluido da lista times para evitar repetições
@@ -171,13 +181,8 @@ function cdbOitavas(){
 
 function cdbQuartas(){
     passarFazerCdb();
-
-    // #jogos1{
-    //     border-end-end-radius: 0;
-    //     border-end-start-radius: 0;
-    //     padding-bottom: 0;
-    // }
     
+    //retira uma das div jogos
     jogosDiv = document.querySelectorAll('.jogos');
     jogosDiv[0].style.borderRadius = '8px';
     jogosDiv[0].style.padding = '30px 0';
@@ -187,6 +192,7 @@ function cdbQuartas(){
 function cdbSemis(){
     cdbQuartas();
 
+    //retira duas div jogo
     jogoDiv = document.querySelectorAll('.jogo');
     jogoDiv[2].style.display = 'none';
     jogoDiv[3].style.display = 'none';
@@ -195,6 +201,7 @@ function cdbSemis(){
 function cdbFinal(){
     cdbSemis();
     
+    //retira uma div jogo
     jogoDiv = document.querySelectorAll('.jogo');
     jogoDiv[1].style.display = 'none';
 }
@@ -203,12 +210,15 @@ function cdbCampeao(){
     cdbFinal();
     const listaTimes = localStorage.getItem('listaTimes').split(',');
 
+    //retira a div jogos
     jogosDiv = document.querySelectorAll('.jogos');
     jogosDiv[0].style.display = 'none';
 
+    //ativa a div de campeão
     campeaoDiv = document.querySelector('#campeao');
     campeaoDiv.style.display = 'flex';
 
+    //adicionar o id do campeão
     timeCampeao = document.querySelector('.timeCampeao');
     timeCampeao.id = listaTimes[0];
 
@@ -219,6 +229,7 @@ function cdbCampeao(){
     carregarImagensTimes('.timeCampeao');
 }
 
+//define no html os times e o primeiro batedor
 function timesJogo(){
     times = document.querySelectorAll('.time');
     nomeBatedor = document.querySelector('#nomeBatedor');
@@ -226,14 +237,17 @@ function timesJogo(){
     
     nomeBatedor.innerHTML = listaTimes[idPlayer][1];
 
+    //define os time no placar
     times[0].id = localStorage.getItem('timePlayer');
     times[1].id = localStorage.getItem('adversario');
+    //define os times na tela final
     times[2].id = localStorage.getItem('timePlayer');
     times[3].id = localStorage.getItem('adversario');
     
     carregarImagensTimes('.time');
 }
 
+//altena entre a vez do player e adversário
 function mudarVez(){
     frase = document.querySelector('#fraseVez');
     placar = document.querySelectorAll('.contagemAcertos');
@@ -242,6 +256,7 @@ function mudarVez(){
     idPlayer = obterIdNumerico(localStorage.getItem('timePlayer'));
     idAdversario = obterIdNumerico(localStorage.getItem('adversario'));
 
+    //se o ultimo a chutar foi o player define a frase e o nome do adversário, e vice-versa
     if(vez=='player'){
         vez='adversario';
         frase.innerHTML = 'ESCOLHA UM LUGAR PARA DEFENDER';
@@ -251,21 +266,28 @@ function mudarVez(){
         frase.innerHTML = 'ESCOLHA UM LUGAR PARA CHUTAR';
         nomeBatedor.innerHTML = listaTimes[idPlayer][((qtdAtualBatidas+0.5)%5)+1];
     }
+    //atualiza o placar
     placar[0].innerHTML = contagemPlayer;
     placar[1].innerHTML = contagemAdversario;
     qtdAtualBatidas = qtdAtualBatidas + 0.5;
 }
 
+//analisa se foi gol
 function analisarChute(posPlayer){
     narracao = document.querySelector('#narracaoTexto');
 
+    //sorteio o lugar do adversário
     posAdversario = Math.round(Math.random()*5)+1;
     
+    //verifica quem chutou
     if(vez=='player'){
+        //reseta o placar caso já tenha tido 5 chutes de cada
         if(qtdGeralBatidas!=5 && (qtdGeralBatidas-1)%5==0){
             contagemPlayer = '';
             contagemAdversario = '';
         }
+
+        //verifica se o player acertou ou não
         if(posPlayer==posAdversario){
             contagemPlayer = contagemPlayer + ' X';
             narracao.innerHTML = 'DEFENDEUUUUU, GOLEIRO BUSCOU NO '+posAdversario;
@@ -275,14 +297,17 @@ function analisarChute(posPlayer){
             narracao.innerHTML = 'Chute no '+posPlayer+' e goleiro no '+posAdversario;
         }
     }else if(vez=='adversario'){
+        //verifica se o adversário acertou ou não
         if(posPlayer==posAdversario){
             contagemAdversario = contagemAdversario + ' X';
-        narracao.innerHTML = 'DEFENDEUUUUU, GOLEIRO BUSCOU NO '+posPlayer;
+            narracao.innerHTML = 'DEFENDEUUUUU, GOLEIRO BUSCOU NO '+posPlayer;
         }else{
             golsAdversario++;
             contagemAdversario = contagemAdversario + ' O';
             narracao.innerHTML = 'Chute no '+posAdversario+' e goleiro no '+posPlayer;
         }
+
+        //quando está em desempate a verificação se alguem venceu é feita após o o chute do adversário
         if(desempate=='ativado'){
             if(golsPlayer>golsAdversario){
                 fimJogo('player');
@@ -292,13 +317,16 @@ function analisarChute(posPlayer){
         }
     }
 
+    //verifica se empatou após as 5 batidas de cada
     if(qtdAtualBatidas>=4.5 && golsAdversario==golsPlayer){
         
         qtdGeralBatidas++;
         desempate = 'ativado';
     }
     
+    //verifica quem venceu
     if(desempate=='desativado'){
+        //calculo permite saber se alguem venceu antes do fim das 5 batidas
         if((golsPlayer-golsAdversario)>(qtdGeralBatidas-qtdAtualBatidas)){
             fimJogo('player');
         }else if((golsAdversario-golsPlayer)>(qtdGeralBatidas-qtdAtualBatidas)){
@@ -309,6 +337,7 @@ function analisarChute(posPlayer){
     mudarVez();
 }
 
+//determina o vencedor e mostra a tela final
 function fimJogo(vencedor){
     localStorage.setItem('vencedorJogo', vencedor);
 
